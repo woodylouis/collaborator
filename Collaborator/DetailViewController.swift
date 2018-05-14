579082dd2336eea1d3970f70af60c728bcabbe64
zswenjin <zswenjin.li@gmail.com>
2018-05-14 16:54:21 +1000

    configure detail view

--- Collaborator/DetailViewController.swift
+++ Collaborator/DetailViewController.swift
@@ -14,11 +14,18 @@ protocol DetailViewControllerDelegate {
 }
 
 let subsectionHeaders = ["TASK", "COLLABORATORS", "LOG"]
+enum subSections: Int {
+    case taskSection = 0
+    case collaboratorSection = 1
+    case logSection = 2
+}
 
 class DetailViewController: UITableViewController, UITextFieldDelegate {
 
+    var objects = subsectionHeaders.map { (_: String) -> [Task] in return [Task]() } //.map
     var delegate: DetailViewControllerDelegate?
-    @IBOutlet weak var detailDescriptionField: UITextField!
+    var indexPath: IndexPath?
+    //@IBOutlet weak var detailDescriptionField: UITextField!
     
     
     public func textFieldShouldReturn(_ textField: UITextField) -> Bool{
@@ -30,8 +37,9 @@ class DetailViewController: UITableViewController, UITextFieldDelegate {
     
     func configureView() {
         // Update the user interface for the detail item.
+        let taskCell = tableView.dequeueReusableCell(withIdentifier: "task") as! TextFiledTableViewCell
         if let detail = detailItem {
-            if let field = detailDescriptionField{
+            if let field = taskCell.taskDetailCell {
                 field.text = detail.taskName
             }
         }
@@ -49,8 +57,10 @@ class DetailViewController: UITableViewController, UITextFieldDelegate {
     }
 
     override func viewWillDisappear(_ animated: Bool) {
-        detailItem?.taskName = detailDescriptionField.text ?? ""
+        //detailItem?.taskName = detailDescriptionField.text ?? ""
+        configureView()
         delegate?.detailViewControllerDidUpdate(self)
+        
     }
     
     var detailItem: Task? {
@@ -59,11 +69,50 @@ class DetailViewController: UITableViewController, UITextFieldDelegate {
             configureView()
         }
     }
-
+    
+    // MARK: - table View
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return subsectionHeaders.count
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // return 1
+        return objects[section].count
+    }
+    
+    // Switch section by identifier
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let identifier: String
+        guard let subSection = subSections(rawValue: indexPath.section) else {
+            fatalError("Unexpectedly got \(indexPath.section)")
+        }
+        switch subSection {
+        case .taskSection:
+            identifier = "task"
+        case .collaboratorSection:
+            identifier = "collaborator"
+        case .logSection:
+            identifier = "log"
+            //        default:
+            //            fatalError("Unexpectedly got \(indexPath.section)")
+        }
+        
+        //       let cell = tableView.dequeueReusableCell(withIdentifier: "task", for: indexPath) as! TextFiledTableViewCell
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath) as! TextFiledTableViewCell
+        let textFiled: UITextField = cell.taskDetailCell
+        textFiled.text = detailItem?.taskName
+        textFiled.delegate = self
+        return cell
+    }
+    
     // MARK: - to show sub header i.e. TASK, COLLABORATORS, LOG
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return subsectionHeaders[section]
     }
+    
+    
     
 }
 
