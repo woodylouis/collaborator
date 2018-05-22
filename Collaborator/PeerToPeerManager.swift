33dbb4fea5fd986db702e515b19f2d7a7795ea62
zswenjin <zswenjin.li@gmail.com>
2018-05-22 11:28:05 +1000

    add peer to peer manager

---
+++ Collaborator/PeerToPeerManager.swift
@@ -0,0 +1,90 @@
+//
+//  PeerToPeerManager.swift
+//  Collaborator
+//
+//  Created by WENJIN LI on 22/5/18.
+//  Copyright © 2018 Wenjin Li. All rights reserved.
+//
+
+import UIKit
+import MultipeerConnectivity
+
+class PeerToPeerManager: NSObject {
+    static let serviceType = "Collaboration-chitchat"
+    
+    private let peerID = MCPeerID(displayName: "Wenjin Li")
+    private let serviceAdvertiser: MCNearbyServiceAdvertiser
+    private let serviceBrowser: MCNearbyServiceBrowser
+    
+    override init() {
+        let service = PeerToPeerManager.serviceType
+        serviceAdvertiser = MCNearbyServiceAdvertiser(peer: peerID, discoveryInfo: [peerID.displayName : UIDevice.current.name], serviceType: service)
+        serviceBrowser = MCNearbyServiceBrowser(peer: peerID, serviceType: service)
+        super.init()
+        serviceAdvertiser.delegate = self
+        serviceAdvertiser.startAdvertisingPeer()
+        serviceBrowser.delegate = self
+    }
+    
+    deinit {
+        serviceAdvertiser.stopAdvertisingPeer()
+    }
+    
+    // session to be established whenever is needed
+    lazy var session: MCSession = {
+        let session = MCSession(peer: peerID, securityIdentity: nil, encryptionPreference: .required)
+        session.delegate = self
+        return session
+    }()
+    
+    func invite(peer: MCPeerID, timeout t: TimeInterval = 10) {
+        print("inviting \(peer.displayName)")
+        serviceBrowser.invitePeer(peer, to: session, withContext: nil, timeout: t)
+    }
+}
+
+extension PeerToPeerManager: MCNearbyServiceAdvertiserDelegate {
+    func advertiser(_ advertiser: MCNearbyServiceAdvertiser, didReceiveInvitationFromPeer peerID: MCPeerID, withContext context: Data?, invitationHandler: @escaping (Bool, MCSession?) -> Void) {
+        print("did recieve invitation from \(peerID)")
+        invitationHandler(true, session)
+    }
+    
+    
+}
+
+extension PeerToPeerManager: MCSessionDelegate {
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        print("did recieve \(data.count) bytes")
+    }
+    
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        print("sessionDidChange \(state)")
+    }
+    
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+        print("didRecievedStream \(streamName)")
+    }
+
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+        print("didRecievedResource \(resourceName)")
+    }
+    
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+        print("finishRecievedResource \(resourceName)")
+    }
+}
+
+extension PeerToPeerManager: MCNearbyServiceBrowserDelegate {
+    func browser(_ browser: MCNearbyServiceBrowser, foundPeer peerID: MCPeerID, withDiscoveryInfo info: [String : String]?) {
+        print("found \(peerID.displayName)")
+        invite(peer: peerID)
+    }
+    
+    func browser(_ browser: MCNearbyServiceBrowser, lostPeer peerID: MCPeerID) {
+        print("lost \(peerID.displayName)")
+    }
+    
+    
+    
+}
+
---
+++ Collaborator/taskEntry.swift
@@ -0,0 +1,12 @@
+//
+//  taskEntry.swift
+//  Collaborator
+//
+//  Created by WENJIN LI on 21/5/18.
+//  Copyright © 2018 Wenjin Li. All rights reserved.
+//
+
+import Foundation
+class TaskEntry {
+    var taskList: [Task] = []
+}
